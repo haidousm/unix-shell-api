decf3fc5caf859544e99583ee12ba2f11476c87f
haidousm <haidous.m@gmail.com>
2021-10-10 15:45:23 +0300

    add cors using rocket_cors

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,57 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
-mod routes;
 
-use routes::execute::execute_cmd;
+use rocket::http::Method;
+use rocket_contrib::json::Json;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+use serde::{Deserialize, Serialize};
+use std::process::Command;
+
+#[derive(Deserialize)]
+pub struct CommandRequest<'r> {
+    command: &'r str,
+}
+
+#[derive(Serialize)]
+pub struct CommandResponse {
+    status: u8,
+    command_output: String,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "nothing to see here.."
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
+#[post("/", format = "application/json", data = "<command_request>")]
+pub fn execute_cmd(command_request: Json<CommandRequest>) -> Json<CommandResponse> {
+    let command_output = Command::new("../c/shell")
+        .current_dir("resources/demo-directory")
+        .arg(command_request.command)
+        .output()
+        .expect("ugh c is sometimes just a b");
+    Json(CommandResponse {
+        status: 200,
+        command_output: String::from_utf8(command_output.stdout)
+            .expect("ugh utf8 is sometimes just a b"),
+    })
+}
+
+fn main() {
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true);
+
+    rocket::ignite()
         .mount("/", routes![index])
         .mount("/execute", routes![execute_cmd])
+        .attach(cors.to_cors().unwrap())
+        .launch();
 }
